0abffe9ab8439737a7e5bdffa74c3d207a5bd454
shaileshs <shaileshs@cantahealth.com>
2016-04-27 19:29:34 +0530

    x

--- config/environmentVariables.js
+++ config/environmentVariables.js
@@ -3,7 +3,7 @@
 // eslint disable no-var
 
 var environmentVariables = {
-  "FOCUS_MONGO_CONNECTION_STRING": "mongodb://10.18.6.112:27017/practice",
+  "FOCUS_MONGO_CONNECTION_STRING": "mongodb://10.18.6.112:27017/demo",
   "FOCUS_REDIS_HOST": "",
   "FOCUS_REDIS_PORT": "",
   "FOCUS_LOGGING_LEVEL": "debug"
