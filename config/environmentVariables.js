47f18b4dee9f249774f236dbf8239068c04233ec
sharadks <sksaraswat@cantahealth.com>
2016-08-04 12:50:09 +0530

    LocalConfiguration

--- config/environmentVariables.js
+++ config/environmentVariables.js
@@ -3,11 +3,11 @@
 // eslint disable no-var
 
 var environmentVariables = {
-  "FOCUS_MONGO_CONNECTION_STRING": "mongodb://10.18.6.109:27017/fhir",
-  "FOCUS_REDIS_HOST": "localhost",
-  "FOCUS_REDIS_PORT": "6379",
-  "FOCUS_LOGGING_LEVEL": "debug",
-  "AUTH_SECRET_KEY": "45a3c06e-ab7e-4256-9e9c-da2ac168ef25"
+  "FOCUS_MONGO_CONNECTION_STRING": process.env.FOCUS_MONGO_CONNECTION_STRING || "mongodb://10.18.6.110:27017/fhir_eccpa",
+  "FOCUS_REDIS_HOST": process.env.FOCUS_REDIS_HOST || "localhost",
+  "FOCUS_REDIS_PORT": process.env.FOCUS_REDIS_PORT || "6379",
+  "FOCUS_LOGGING_LEVEL": process.env.FOCUS_LOGGING_LEVEL || "debug",
+  "FOCUS_AUTH_SECRET_KEY": process.env.FOCUS_AUTH_SECRET_KEY || "45a3c06e-ab7e-4256-9e9c-da2ac168ef25"
 };
 
 module.exports = environmentVariables;
